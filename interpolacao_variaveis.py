aa947e993aca72d0056072cc4ebf7567c8bd4dc9
Bruno César <brunimptc2009@hotmail.com>
2025-07-12 14:43:13 -0300

    Introduce string formatting examples

--- interpolacao_variaveis.py
+++ interpolacao_variaveis.py
@@ -12,3 +12,10 @@ print(" \n Olá, me chamo %s. Eu tenho %d anos de idade, trabalho como %s e esto
 
 # metodo format 
 print(" \n Olá, me chamo {}. Eu tenho {} anos de idade, trabalho como {} e estou matriculado no curso de {}. \n ".format(nome, idade, profissao, linguagem))
+
+# metodo format pelo indice
+print(" \n Olá, me chamo {3}. Eu tenho {2} anos de idade, trabalho como {1} e estou matriculado no curso de {0}. \n ".format(linguagem, profissao, idade, nome))
+
+# direto
+print(" \n Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matriculado no curso de {linguagem}. \n ")
+
