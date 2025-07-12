5529335d5e1fd2c7431856fc83cef13e033f7abf
Bruno César <brunimptc2009@hotmail.com>
2025-07-12 14:43:13 -0300

    Add file for string slicing examples

---
+++ fatiandostring.py
@@ -0,0 +1,16 @@
+nome = "Bruno César Alves"
+
+print ( "%s" %    (nome[0]))
+
+print ( "%s" %    (nome[:5]))
+
+print ( "%s" %    (nome[5:]))
+
+print ( "%s" %    (nome[5:11]))
+
+print ( "%s" %    (nome[5:11:2]))   # vai recupera de 2 em 2
+
+print ( "%s" %    (nome[:]))
+
+print ( "%s" %    (nome[:: -1]))    # espelha string -1 vai ser invertida
+
