f426efc0360082a001300d1209c98c6d15ea3272
Bruno César <brunimptc2009@hotmail.com>
2025-07-21 11:36:15 -0300

    pronto

--- Desafio Banco/main.py
+++ Desafio Banco/main.py
@@ -12,28 +12,29 @@ limite = 0
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
-valor = 0
-deposito = 0
-
+saque = 0
 while True:
     
     opçao = input(menu)
     
     if opçao == "1":
-        print("\n Deposito \n")
-        deposito = float(input("Digite o quanto voce vai depositar:"))
-        if deposito > 0: 
-            saldo += deposito 
-            extrato += f"\nDeposito: R${deposito:.2f}\n"
-            print ("Deposito bem sucessido! seu saldo agora é R${:.2f}, e esse é o seu extrato:{}".format(saldo, extrato))
-        elif deposito <= 0:
-            print ("Voce digitou um valor negativo")
-           
-    elif opçao == "2":
-        print("\n sacar \n")
-      
-    elif opçao == "3":
-        print("Extrato")
+        valor = float(input("Informe o valor do deposito:  "))
         
+        if valor > 0: 
+            saldo += valor 
+            extrato += f"\nDepósito: R${valor:.2f}"
+            print ("Deposito bem sucessido! seu saldo agora é R${:.2f}, e esse é o seu extrato:{}".format(saldo, extrato))
+        elif valor <= 0:
+            print ("Voce digitou um valor negativo") 
+                  
+    elif opçao == "2":
+        saque = float(input("Digite o quanto voce vai sacar: "))
+        saldo -= saque
+        extrato += f"\nSaque: R${saque:.2f}"
+        print ("Saque bem sucessido! seu saldo agora é R${:.2f}, e esse é o seu extrato: {}".format(saldo, extrato))
+        
+    elif opçao == "3":
+        print("extrato: {}".format(extrato))
+             
     elif opçao == "4":
         break
